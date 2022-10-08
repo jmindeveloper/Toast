5a65f04818a1d7533bfa9fddd8b7a4d5a9577452
jmindeveloper <jmin.developer@gmail.com>
2022-10-08 15:12:18 +0900

    init

---
+++ Toast/Toast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Toast
+//
+//  Created by J_Min on 2022/10/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
